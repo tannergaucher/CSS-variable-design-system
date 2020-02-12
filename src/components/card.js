d1cb03d1dbea3877c6fa7fb76b5095661fd55e82
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-02-12 11:19:24 -0500

    Update card styles

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ const Card = () => (
   <div className="card">
     <h4 className="card-title">I'm a Card Title</h4>
     <Image />
-    <p className="card-text">
+    <p className="card-text text--sm">
       Card text is a paragraph element with a class of card dash text.
     </p>
   </div>
